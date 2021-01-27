b724e3f95933162003e876a7d02d1478c476f028
AndrewBoshra <andrew.boshr@gmail.com>
2021-01-27 15:45:54 +0200

    final simulation

--- collision.py
+++ collision.py
@@ -1,7 +1,11 @@
 import pygame,random,sys
 
 
-gravity=.05
+GRAVITY=.05
+SWIDTH=int(input("Enter Screen Width:"))
+SHEIGHT=int(input("Enter Screen Height:"))
+BALLSNUM=int(input("Enter Balls Number:"))
+COLLISIONFACTOR=.98
 class ball:
     def __init__(self,x,y,radius,color):
         self.x=x
@@ -13,27 +17,29 @@ class ball:
         pygame.draw.circle(screen,self.color,(self.x,self.y),self.radius)
     def updatePos(self):
         #next position 
-        if self.y+self.speedy+self.radius>=720:
-            self.y=720-self.radius
-            self.speedy*=-.98
+        if self.y+self.speedy+self.radius>=SHEIGHT:
+            self.y=SHEIGHT-self.radius
+            self.speedy*=-1*COLLISIONFACTOR
         else:
-            self.speedy+=gravity
+            self.speedy+=GRAVITY
             self.y+=self.speedy
 
 pygame.init()
-screen=pygame.display.set_mode((1280,720))
+screen=pygame.display.set_mode((SWIDTH,SHEIGHT))
 balls=[]
-for _ in range(200):
+for _ in range(BALLSNUM):
     radius=random.randint(15,40)
-    print(radius)
-    x=random.randrange(radius,1280-radius)
-    y=random.randrange(radius,720-radius)
+    x=random.randrange(radius,SWIDTH-radius)
+    y=random.randrange(radius,SHEIGHT-radius)
     color=(random.randrange(20,255),random.randrange(20,255),random.randrange(20,255))
     ball_gen=ball(x,y,radius,color)
     balls.append(ball_gen)
 
+print("Simulation Started press any key to stop")
 while True:
-
+    for event in pygame.event.get():
+        if event.type in (pygame.QUIT , pygame.KEYDOWN):
+            sys.exit()
     for ball in balls:
         ball.updatePos()
     screen.fill((0,0,50))
